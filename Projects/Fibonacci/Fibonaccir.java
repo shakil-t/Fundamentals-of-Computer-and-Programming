d780969cd737b4f5b413ff51f49b0e02e957f4f7
Shakil <tayefehshakila@gmail.com>
2021-11-28 09:18:08 +0100

    Add files via upload

---
+++ Projects/Fibonacci/Fibonaccir.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fibonaccir;
+
+/**
+ *
+ * @author shakil
+ */
+import java.util.Scanner;
+public class Fibonaccir {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static int n1=1,n2=1,n3=0;
+    public static void fibonaccir(int counter){
+        if(counter>0){
+            n3=n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.println(n3);
+            fibonaccir(counter-1);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input=new Scanner(System.in);
+        System.out.println(" Enter a number ");
+        int counter;
+        counter = input.nextInt();
+        System.out.println(n1);
+        System.out.println(n2);
+        fibonaccir(counter-2);
+        
+    }
+    
+}
+      
