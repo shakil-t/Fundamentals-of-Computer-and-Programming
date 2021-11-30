010ca5f26c26ea327c17f50262fdd5038ee4c36a
Shakil <tayefehshakila@gmail.com>
2021-11-30 13:42:42 +0100

    Create Interface.java

---
+++ Projects/Interface/Interface.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkginterface;
+
+/**
+ *
+ * @author Shakila
+ */
+public class Interface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Starting to learn OOP with the example of rectangles
+        Rectangle a=new Rectangle();
+        a.getArea(2, 3);
+        a.getPerimeter(2, 3);
+        a.Resize(2, 3, 2);
+        a.getArea(2, 3);
+        a.getPerimeter(2, 3);
+    }
+    
+}
