b53058012592f3db79df048375f293aeb8bf1f5d
Shakil <tayefehshakila@gmail.com>
2021-11-30 13:55:44 +0100

    Update Array3.java

--- Projects/Array/Array3.java
+++ Projects/Array/Array3.java
@@ -15,30 +15,30 @@ public class Array3 {
     /**
      * @param args the command line arguments
      */
-    public static double getMax(double[] array){
-        double max=array[0];
-        for(int i=1;i<array.length;i++){
-            if(array[i]>max){
-                max=array[i];
+    public static double getMax(double[] array) {
+        double max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
             }
         }
         return max;
     }
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner input=new Scanner(System.in);
-        double[] array=new double[20];
+        Scanner input = new Scanner(System.in);
+        double[] array = new double[20];
         System.out.println(" Enter the numbers ");
-        for(int i=0;i<20;i++){
+        for (int i = 0; i < 20; i++) {
             array[i] = input.nextDouble();
         }
-        double max=getMax(array);
+        double max = getMax(array);
         System.out.println(" Max value is " + max);
-        for(int j=0;j<20;j++){
-            if(array[j]==max){
+        for (int j = 0; j < 20; j++) {
+            if (array[j] == max) {
                 System.out.println(j);
             }
         }
     }
-    
+
 }
