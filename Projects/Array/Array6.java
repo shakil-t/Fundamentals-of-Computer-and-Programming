54f6b2fccd6b78e2bcad97cb563ca2c3238f40b4
Shakil <tayefehshakila@gmail.com>
2021-11-30 13:57:24 +0100

    Update Array6.java

--- Projects/Array/Array6.java
+++ Projects/Array/Array6.java
@@ -17,39 +17,39 @@ public class Array6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner input=new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int s1;
         System.out.println(" Enter the size of the first array ");
         s1 = input.nextInt();
         int s2;
         System.out.println(" Enter the size of the second array ");
         s2 = input.nextInt();
-        double[] array1=new double[s1];
+        double[] array1 = new double[s1];
         System.out.println(" Enter the elements of the first array ");
-        for(int a=0;a<s1;a++){
+        for (int a = 0; a < s1; a++) {
             array1[a] = input.nextDouble();
         }
-        double[] array2=new double[s2];
+        double[] array2 = new double[s2];
         System.out.println(" Enter the elements of the second array ");
-        for(int b=0;b<s2;b++){
+        for (int b = 0; b < s2; b++) {
             array2[b] = input.nextDouble();
         }
-        double[] array3=new double[s1+s2];
-        int i=0,j=0,k=0;
-        while(i<s1&&j<s2){
-            if(array1[i]>array2[j]){
-                array3[k]=array1[i];
+        double[] array3 = new double[s1 + s2];
+        int i = 0, j = 0, k = 0;
+        while (i < s1 && j < s2) {
+            if (array1[i] > array2[j]) {
+                array3[k] = array1[i];
                 k++;
                 i++;
-            }else{
-                array3[k]=array2[j];
+            } else {
+                array3[k] = array2[j];
                 k++;
                 j++;
             }
         }
-        for(int c=0;c<array3.length;c++){
+        for (int c = 0; c < array3.length; c++) {
             System.out.println(array3[c]);
         }
     }
-    
+
 }
