bae67900331281429ae28f079c89f0f2a04c3958
Shakil <tayefehshakila@gmail.com>
2021-11-30 14:07:51 +0100

    Update Rectangle.java

--- Projects/Interface/Rectangle.java
+++ Projects/Interface/Rectangle.java
@@ -9,24 +9,24 @@ package pkginterface;
  *
  * @author Shakila
  */
-public class Rectangle implements Resizeable{
+public class Rectangle implements Resizeable {
 
     @Override
     public void Resize(double w, double h, double x) {
-        w=w*x;
-        h=h*x;
-        System.out.println(" New width :"+w);
-        System.out.println(" New height :"+h);
+        w = w * x;
+        h = h * x;
+        System.out.println(" New width :" + w);
+        System.out.println(" New height :" + h);
     }
 
     @Override
     public void getArea(double w, double h) {
-        System.out.println(" Area :"+w*h);
+        System.out.println(" Area :" + w * h);
     }
 
     @Override
     public void getPerimeter(double x, double y) {
-        System.out.println(" Perimeter :"+2*(x+y));
+        System.out.println(" Perimeter :" + 2 * (x + y));
     }
-    
+
 }
