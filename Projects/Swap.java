0d6c5dc20cd0aaefcf963968aa13805d92622018
Shakil <tayefehshakila@gmail.com>
2021-11-30 14:15:34 +0100

    Update Swap.java

--- Projects/Swap.java
+++ Projects/Swap.java
@@ -15,36 +15,36 @@ public class Swap {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        int a=23;
-        int b=75;
+        // Some ways to swap two numbers
+        int a = 23;
+        int b = 75;
         //swapping by a third variable
         int t;
-        t=a;
-        a=b;
-        b=t;
-        System.out.println("a="+a);
-        System.out.println("b="+b);
+        t = a;
+        a = b;
+        b = t;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
         System.out.println();
-        
-        int c=6;
-        int d=2;
+
+        int c = 6;
+        int d = 2;
         //swapping without third variable
-        c=c+d;
-        d=c-d;
-        c=c-d;
-        System.out.println("c="+c);
-        System.out.println("d="+d);
+        c = c + d;
+        d = c - d;
+        c = c - d;
+        System.out.println("c=" + c);
+        System.out.println("d=" + d);
         System.out.println();
-        
-        int e=19;
-        int f=7;
+
+        int e = 19;
+        int f = 7;
         //swapping by logical operators
-        e=e^f;
-        f=e^f;
-        e=e^f;
-        System.out.println("e="+e);
-        System.out.println("f="+f);
+        e = e ^ f;
+        f = e ^ f;
+        e = e ^ f;
+        System.out.println("e=" + e);
+        System.out.println("f=" + f);
     }
-    
+
 }
