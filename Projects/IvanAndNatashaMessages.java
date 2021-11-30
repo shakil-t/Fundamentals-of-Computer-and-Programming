4f3c2b420429cc498aeb3da7251623b73e2f9b40
Shakil <tayefehshakila@gmail.com>
2021-11-30 14:11:57 +0100

    Update IvanAndNatashaMessages.java

--- Projects/IvanAndNatashaMessages.java
+++ Projects/IvanAndNatashaMessages.java
@@ -14,34 +14,36 @@ public class IvanAndNatashaMessages {
     /**
      * @param args the command line arguments
      */
-    public static boolean isPalindrome(int length){
-        String number=String.valueOf(length);
-        String reverse="";
-        for(int i=number.length()-1;i>=0;i--){
-            reverse+=number.charAt(i);
+    public static boolean isPalindrome(int length) {
+        String number = String.valueOf(length);
+        String reverse = "";
+        for (int i = number.length() - 1; i >= 0; i--) {
+            reverse += number.charAt(i);
         }
-        if(number.equals(reverse)){
+        if (number.equals(reverse)) {
             return true;
         }
         return false;
     }
     public static void main(String[] args) {
         // TODO code application logic here
-        String keyboard="qwerBtyui>op)asdf(ghjkl;zxc3vb<nm,.:/";
-        String[] messages={"djsl>;;pbrdupinv",
-                           "lrr)3s;,smfrsy)r3j)r3je>yjdj>Blslspi/a",
-                           ";pbr>d;pbr",
-                           "s;;upimrrf>d;pbr"};
-        for(int i=0;i<messages.length;i++){
-            boolean result=isPalindrome(messages[i].length());
-            String originalMessage="";
-            for(int j=0;j<messages[i].length();j++){
-                for(int k=0;k<keyboard.length();k++){
-                    if(messages[i].charAt(j)==keyboard.charAt(k)){
-                        if(result){
-                            originalMessage+=keyboard.charAt(k+1);
-                        }else{
-                            originalMessage+=keyboard.charAt(k-1);
+        String keyboard = "qwerBtyui>op)asdf(ghjkl;zxc3vb<nm,.:/";
+        String[] messages = {
+            "djsl>;;pbrdupinv",
+            "lrr)3s;,smfrsy)r3j)r3je>yjdj>Blslspi/a",
+            ";pbr>d;pbr",
+            "s;;upimrrf>d;pbr"
+        };
+        for (int i = 0; i < messages.length; i++) {
+            boolean result = isPalindrome(messages[i].length());
+            String originalMessage = "";
+            for (int j = 0; j < messages[i].length(); j++) {
+                for (int k = 0; k < keyboard.length(); k++) {
+                    if (messages[i].charAt(j) == keyboard.charAt(k)) {
+                        if (result) {
+                            originalMessage += keyboard.charAt(k + 1);
+                        } else {
+                            originalMessage += keyboard.charAt(k - 1);
                         }
                     }
                 }
@@ -49,6 +51,5 @@ public class IvanAndNatashaMessages {
             System.out.println(originalMessage);
         }
     }
-    
-}
 
+}
