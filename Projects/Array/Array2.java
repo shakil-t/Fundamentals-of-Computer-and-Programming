b23ed4b5124a5cac72a39b0eb774d4821dfc7db7
Shakil <tayefehshakila@gmail.com>
2021-11-30 13:54:44 +0100

    Update Array2.java

--- Projects/Array/Array2.java
+++ Projects/Array/Array2.java
@@ -17,23 +17,21 @@ public class Array2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner input=new Scanner(System.in);
-        double[] array=new double[100];
+        Scanner input = new Scanner(System.in);
+        double[] array = new double[100];
         System.out.println(" Enter the numbers ");
-        for(int i=0;i<100;i++){
+        for (int i = 0; i < 100; i++) {
             array[i] = input.nextDouble();
         }
-        for(int j=0;j<100;j++){
-            if(array[j]>=0){
+        for (int j = 0; j < 100; j++) {
+            if (array[j] >= 0) {
                 System.out.println(array[j]);
             }
         }
-            for(int k=0;k<100;k++){
-                if(array[k]<0){
-                    System.out.println(array[k]);
-                }
+        for (int k = 0; k < 100; k++) {
+            if (array[k] < 0) {
+                System.out.println(array[k]);
             }
         }
     }
-    
-
+}
