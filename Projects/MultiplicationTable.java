d513fffadd50e7a24776267d9d4d564f733d880d
Shakil <tayefehshakila@gmail.com>
2021-11-30 14:18:10 +0100

    Create MultiplicationTable.java

---
+++ Projects/MultiplicationTable.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplicationtable;
+
+/**
+ *
+ * @author Shakila
+ */
+public class MultiplicationTable {
+    public static void MultiplicationTable(int row, int column) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
+                System.out.printf(" %3d ", i * j);
+            }
+            System.out.println();
+        }
+        return;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This code prints a n*n multiplication table
+        MultiplicationTable(10, 10);
+    }
+}
